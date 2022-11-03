86f757cc9e20f541b54e2d8dce22928bfdef533f
Daniel <danielvalladoorozco@gmail.com>
2022-11-03 06:33:03 -0600

    Primera entrega

---
+++ Sistema de registro/src/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+import vista.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EliminarFuncion login = new EliminarFuncion();
+        login.setVisible(true);
+    }  
+}
